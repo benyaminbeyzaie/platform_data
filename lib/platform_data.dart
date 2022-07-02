09351efa74778b4c5091087ebeb84ae15ce3b8d3
Benyamin Beyzaie <benyamin.beyzaie@hezardastan.net>
2022-07-02 12:15:06 +0430

    add getters and better enums

--- lib/platform_data.dart
+++ lib/platform_data.dart
@@ -3,19 +3,37 @@ library platform_data;
 import 'dart:io' show Platform;
 import 'package:flutter/foundation.dart' show kIsWeb;
 
-enum PlatformType { WEB, ANDROID, IOS, WINDOWS, LINUX, MAC, UNKNOWN }
+enum PlatformType { web, android, ios, windows, linux, mac, unknown }
 
 class PlatformData {
-  static PlatformType platformType = PlatformType.UNKNOWN;
+  static PlatformType platformType = PlatformType.unknown;
   static init() {
     if (kIsWeb) {
-      platformType = PlatformType.WEB;
+      platformType = PlatformType.web;
     } else {
-      if (Platform.isAndroid) platformType = PlatformType.ANDROID;
-      else if (Platform.isIOS) platformType = PlatformType.IOS;
-      else if (Platform.isLinux) platformType = PlatformType.LINUX;
-      else if (Platform.isMacOS) platformType = PlatformType.MAC;
-      else if (Platform.isWindows) platformType = PlatformType.WINDOWS;
+      if (Platform.isAndroid) {
+        platformType = PlatformType.android;
+      } else if (Platform.isIOS) {
+        platformType = PlatformType.ios;
+      } else if (Platform.isLinux) {
+        platformType = PlatformType.linux;
+      } else if (Platform.isMacOS) {
+        platformType = PlatformType.mac;
+      } else if (Platform.isWindows) {
+        platformType = PlatformType.windows;
+      }
     }
   }
 }
+
+get isPlatformWeb => PlatformData.platformType == PlatformType.web;
+
+get isPlatformAndroid => PlatformData.platformType == PlatformType.android;
+
+get isPlatformIOS => PlatformData.platformType == PlatformType.ios;
+
+get isPlatformLinux => PlatformData.platformType == PlatformType.linux;
+
+get isPlatformMac => PlatformData.platformType == PlatformType.mac;
+
+get isPlatformWindows => PlatformData.platformType == PlatformType.windows;
